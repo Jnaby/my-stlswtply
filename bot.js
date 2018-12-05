7932e0ca97b300034d934620a96a55e6b3c8346b
Jnaby <36682528+Jnaby@users.noreply.github.com>
2018-12-05 14:30:58 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -36,7 +36,7 @@ client.on('ready', () => {
 
 var prefix = "-"
 client.on('message', function(message) {
-    const myID = "505761472232816641";
+    const myID = "323160008411971585";
    let args = message.content.split(" ").slice(1).join(" ");
     if(message.content.startsWith(prefix + "setname")) {
                 if(message.author.id !== myID) return;
